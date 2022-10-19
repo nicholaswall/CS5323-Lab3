de82b5054946dbef2828e60d26cc7e6d22a78a18
Nick Wall <60442663+nicholaswall@users.noreply.github.com>
2022-10-19 13:44:24 -0500

    Initial Commit

---
+++ GameLab/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GameLab
+//
+//  Created by Nick Wall on 10/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
